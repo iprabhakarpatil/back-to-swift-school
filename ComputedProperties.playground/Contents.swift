5d66c6db90fd19ec24957cefa48771b7437c6d2c
Prabhakar Patil <prabhakar@squats.in>
2020-02-12 01:52:34 +0530

    An example of computed properties using enum

--- ComputedProperties.playground/Contents.swift
+++ ComputedProperties.playground/Contents.swift
@@ -4,6 +4,7 @@
 
 // Definition: The computed properties are similar to the stored properties (variables only) with the getter(accessors) and setter(mutators) that can be modified. A computed property cannot be a constant ("'let' declarations cannot be computed properties"). A computed property can be READ ONLY but not WRITE ONLY, as the properties do not actually store values.
 
+import UIKit
 
 struct Time {
     
@@ -40,4 +41,45 @@ time.hours // Prints the hour as 1
 
 time.seconds = 800 // Sets new value of seconds to the seconds.
 time.seconds // Prints 800
+
+
+// Following is the example of the read only computed properties that we can use in the User enum for defining the user title and user icon.
+enum User {
+    case player
+    case referee
+    case manager
+    
+    var userDescription: String {
+        switch self {
+        case .manager: return "Manager"
+        case .player: return "Player"
+        case .referee: return "Referee"
+            
+        }
+        
+    }
+    
+    var userIconImage: String {
+        switch  self {
+        case .manager: return "🧑🏻‍💼"
+        case .player: return "🏃🏻"
+        case .referee: return "👮🏻‍♀️"
+            
+        }
+    }
+}
+
+let manager: User = .manager
+manager.userDescription
+manager.userIconImage
+
+let referee: User = .referee
+referee.userIconImage
+referee.userDescription
+
+let player: User = .player
+player.userIconImage
+player.userDescription
+
+
 
